113f682b082717cee650ccefe987a5370f120f62
Lukas Renggli <renggli@gmail.com>
2018-08-12 18:19:06 +0200

    Add a whole bunch of helpers to add, subtract, and multiply matrices.

--- lib/src/matrix/matrix.dart
+++ lib/src/matrix/matrix.dart
@@ -7,6 +7,9 @@ import 'view/row_view.dart';
 import 'view/sub_matrix.dart';
 import 'view/transposed_matrix.dart';
 
+typedef Matrix<T> MatrixConstructor<T>(
+    DataType<T> dataType, int rowCount, int colCount);
+
 abstract class Matrix<T> {
   /// The data type of this matrix.
   DataType<T> get dataType;
@@ -69,5 +72,133 @@ abstract class Matrix<T> {
       }
     }
     return buffer.toString();
+  }
+
+  /// Helper to copy a matrix from [source].
+  static Matrix<T> copy<T>(
+    Matrix<T> source, {
+    Matrix<T> target,
+    MatrixConstructor<T> constructor,
+    DataType<T> dataType,
+  }) {
+    if (target != null) {
+      if (target.rowCount != source.rowCount ||
+          target.colCount != source.colCount) {
+        throw ArgumentError('Target matrix does not match in size.');
+      }
+    }
+    final result = target ??
+        constructor(
+          dataType ?? source.dataType,
+          source.rowCount,
+          source.colCount,
+        );
+    for (var r = 0; r < result.rowCount; r++) {
+      for (var c = 0; c < result.colCount; c++) {
+        result.setUnchecked(r, c, source.getUnchecked(r, c));
+      }
+    }
+    return result;
+  }
+
+  /// Helper to add two numeric matrices [sourceA] and [sourceB].
+  static Matrix<T> add<T extends num>(
+    Matrix<T> sourceA,
+    Matrix<T> sourceB, {
+    Matrix<T> target,
+    MatrixConstructor<T> constructor,
+    DataType<T> dataType,
+  }) {
+    if (sourceA.rowCount != sourceB.rowCount ||
+        sourceA.colCount != sourceB.colCount) {
+      throw ArgumentError('Source matrices do not match in size.');
+    }
+    if (target != null) {
+      if (target.rowCount != sourceA.rowCount ||
+          target.colCount != sourceA.colCount) {
+        throw ArgumentError('Target matrix does not match in size.');
+      }
+    }
+    final result = target ??
+        constructor(
+          dataType ?? sourceA.dataType,
+          sourceA.rowCount,
+          sourceA.colCount,
+        );
+    for (var r = 0; r < result.rowCount; r++) {
+      for (var c = 0; c < result.colCount; c++) {
+        result.setUnchecked(
+            r, c, sourceA.getUnchecked(r, c) + sourceB.getUnchecked(r, c));
+      }
+    }
+    return result;
+  }
+
+  /// Helper to subtract two numeric matrices [sourceA] and [sourceB].
+  static Matrix<T> sub<T extends num>(
+    Matrix<T> sourceA,
+    Matrix<T> sourceB, {
+    Matrix<T> target,
+    MatrixConstructor<T> constructor,
+    DataType<T> dataType,
+  }) {
+    if (sourceA.rowCount != sourceB.rowCount ||
+        sourceA.colCount != sourceB.colCount) {
+      throw ArgumentError('Source matrices do not match in size.');
+    }
+    if (target != null) {
+      if (target.rowCount != sourceA.rowCount ||
+          target.colCount != sourceA.colCount) {
+        throw ArgumentError('Target matrix does not match in size.');
+      }
+    }
+    final result = target ??
+        constructor(
+          dataType ?? sourceA.dataType,
+          sourceA.rowCount,
+          sourceA.colCount,
+        );
+    for (var r = 0; r < result.rowCount; r++) {
+      for (var c = 0; c < result.colCount; c++) {
+        result.setUnchecked(
+            r, c, sourceA.getUnchecked(r, c) - sourceB.getUnchecked(r, c));
+      }
+    }
+    return result;
+  }
+
+  /// Helper to multiply two numeric matrices [sourceA] and [sourceB].
+  static Matrix<T> mul<T extends num>(
+    Matrix<T> sourceA,
+    Matrix<T> sourceB, {
+    Matrix<T> target,
+    MatrixConstructor<T> constructor,
+    DataType<T> dataType,
+  }) {
+    if (sourceA.colCount != sourceB.rowCount) {
+      throw ArgumentError('Inner dimensions of source matrices do not match.');
+    }
+    if (target != null) {
+      if (target.rowCount != sourceA.rowCount ||
+          target.colCount != sourceB.colCount) {
+        throw ArgumentError('Target matrix does not match in size.');
+      }
+    }
+    final result = target ??
+        constructor(
+          dataType ?? sourceA.dataType,
+          sourceA.rowCount,
+          sourceB.colCount,
+        );
+    for (var r = 0; r < result.rowCount; r++) {
+      for (var c = 0; c < result.colCount; c++) {
+        var sum = result.dataType.nullValue;
+        for (var j = 0; j < sourceA.colCount; j++) {
+          sum += sourceA.getUnchecked(r, j) * sourceB.getUnchecked(j, c);
+        }
+        result.setUnchecked(r, c, sum);
+      }
+    }
+    return result;
   }
 }
