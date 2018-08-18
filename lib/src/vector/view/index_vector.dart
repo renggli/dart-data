178985ef34cf0a2d6a909bc5369b136877ea98a1
Lukas Renggli <renggli@gmail.com>
2018-08-18 10:21:37 +0200

    Introduce indexed vector view.

---
+++ lib/src/vector/view/index_vector.dart
@@ -0,0 +1,31 @@
+library data.vector.view.index_vector;
+
+import 'package:data/type.dart';
+
+import '../../shared/config.dart';
+import '../vector.dart';
+
+class IndexVector<T> extends Vector<T> {
+  final Vector<T> _vector;
+  final List<int> _indexes;
+
+  IndexVector(this._vector, Iterable<int> indexes)
+      : _indexes = indexDataType.copyList(indexes);
+
+  @override
+  DataType<T> get dataType => _vector.dataType;
+
+  @override
+  int get count => _indexes.length;
+
+  @override
+  T getUnchecked(int index) => _vector.getUnchecked(_indexes[index]);
+
+  @override
+  void setUnchecked(int index, T value) =>
+      _vector.setUnchecked(_indexes[index], value);
+
+  @override
+  Vector<T> indexUnchecked(Iterable<int> indexes) =>
+      IndexVector(_vector, indexes.map((index) => _indexes[index]));
+}
