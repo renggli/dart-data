d67cf1abc4bf8e729ae4d91864bfa5dcca3938cf
Lukas Renggli <renggli@gmail.com>
2018-08-19 16:18:34 +0200

    Add operators for matrix norms.

--- lib/src/matrix/decompositions.dart
+++ lib/src/matrix/decompositions.dart
@@ -1,5 +1,8 @@
 library data.matrix.decompositions;
 
+import 'dart:math' as math;
+
+import '../shared/math.dart';
 import 'decomposition/cholesky.dart';
 import 'decomposition/eigen.dart';
 import 'decomposition/lu.dart';
@@ -45,3 +48,30 @@ int rank(Matrix<num> source) => singularValue(source).rank;
 
 /// Returns the condition, the ratio of largest to smallest singular value.
 double cond(Matrix<num> source) => singularValue(source).cond;
+
+/// Returns the one norm, the maximum column sum.
+T norm1<T extends num>(Matrix<T> source) {
+  var result = source.dataType.nullValue;
+  for (var c = 0; c < source.colCount; c++) {
+    var sum = source.dataType.nullValue;
+    for (var r = 0; r < source.rowCount; r++) {
+      sum += source.getUnchecked(r, c).abs();
+    }
+    result = math.max(result, sum);
+  }
+  return result;
+}
+
+/// Returns the two norm, the maximum singular value.
+double norm2(Matrix<num> source) => singularValue(source).norm2;
+
+/// Returns the frobenius norm, the sum of squares of all elements.
+double normF(Matrix<num> source) {
+  var result = 0.0;
+  for (var c = 0; c < source.colCount; c++) {
+    for (var r = 0; r < source.rowCount; r++) {
+      result = hypot(result, source.getUnchecked(r, c));
+    }
+  }
+  return result;
+}
