877a51705af5eefdbc29413dcf407dcaf12be66d
Lukas Renggli <renggli@gmail.com>
2018-09-02 09:39:01 +0200

    Better formatting for magic square example.

--- example/magic_square.dart
+++ example/magic_square.dart
@@ -5,6 +5,7 @@ import 'dart:math' as math;
 import 'package:data/matrix.dart' as matrix;
 import 'package:data/type.dart';
 import 'package:data/vector.dart' as vector;
+import 'package:more/printer.dart' show Printer;
 
 /// Builder for magic matrices.
 final _builder = matrix.Matrix.builder.withType(DataType.int64);
@@ -63,8 +64,17 @@ matrix.Matrix<int> magic(int n) {
   }
 }
 
+Printer fixedWidthIntegerToString(int width) => Printer.fixed().padLeft(width);
+
+Printer fixedWidthDoubleToString(int width, int precision) =>
+    Printer.fixed(precision: precision).padLeft(width);
+
 void main() {
   final eps = math.pow(2.0, -52.0);
+
+  print(
+      '      n     trace       max_eig   rank        cond      lu_res      qr_res');
+  print('');
 
   for (var n = 3; n <= 64; n++) {
     final m = magic(n);
@@ -73,24 +83,33 @@ void main() {
     final buffer = [];
 
     // Order of magic square.
-    buffer.add(n);
+    buffer.add(fixedWidthIntegerToString(7)(n));
 
     // Diagonal sum, should be the magic sum, (n^3 + n)/2.
-    buffer.add(vector.sum(m.diagonal()));
+    {
+      final t = vector.sum(m.diagonal());
+      buffer.add(fixedWidthIntegerToString(10)(t));
+    }
 
     // Maximum eigenvalue of (A + A') / 2, should equal trace.
-    final e =
-        matrix.eigenvalue(matrix.scale(0.5, matrix.add(md, md.transpose)));
-    buffer.add(e.realEigenvalues.last.toStringAsFixed(3));
+    {
+      final e =
+          matrix.eigenvalue(matrix.scale(0.5, matrix.add(md, md.transpose)));
+      buffer.add(fixedWidthDoubleToString(14, 3)(e.realEigenvalues.last));
+    }
 
     // Linear algebraic rank, should equal n if n is odd, be less than n if n
     // is even.
-    buffer.add(matrix.rank(m));
+    {
+      final r = matrix.rank(m);
+      buffer.add(fixedWidthIntegerToString(7)(r));
+    }
 
     // L_2 condition number, ratio of singular values.
     {
       final c = matrix.cond(m);
-      buffer.add(c < 1 / eps ? c.toStringAsFixed(3) : 'Inf');
+      buffer.add(
+          fixedWidthDoubleToString(12, 3)(c < 1 / eps ? c : double.infinity));
     }
 
     // Test of LU factorization, norm1(L*U-A(p,:))/(n*eps).
@@ -101,7 +120,7 @@ void main() {
       final p = lu.pivot;
       final r = matrix.sub(matrix.mul(l, u), md.rowIndex(p));
       final res = matrix.norm1(r) / (n * eps);
-      buffer.add(res.toStringAsFixed(3));
+      buffer.add(fixedWidthDoubleToString(12, 3)(res));
     }
 
     // Test of QR factorization, norm1(Q*R-A)/(n*eps).
@@ -111,9 +130,9 @@ void main() {
       final r = qr.upper;
       final R = matrix.sub(matrix.mul(q, r), m);
       final res = matrix.norm1(R) / (n * eps);
-      buffer.add(res.toStringAsFixed(3));
+      buffer.add(fixedWidthDoubleToString(12, 3)(res));
     }
 
-    print(buffer.join('\t'));
+    print(buffer.join(''));
   }
 }
