e28ee4d082d9c61bed22a805fcc9dd44161bbbe6
Lukas Renggli <renggli@gmail.com>
2018-09-22 11:03:20 +0200

    Cleanup vector operators.

--- lib/src/vector/operators.dart
+++ lib/src/vector/operators.dart
@@ -8,8 +8,8 @@ import 'package:data/type.dart';
 import 'builder.dart';
 import 'vector.dart';
 
-Vector<T> _targetOrBuilder<T>(
-    int count, Vector<T> target, Builder<T> builder, DataType<T> type) {
+Vector<T> _targetOrBuilderOrDataType<T>(
+    int count, Vector<T> target, Builder<T> builder, DataType<T> dataType) {
   if (target != null) {
     if (count != target.count) {
       throw ArgumentError('Expected a vector of dimension $count, '
@@ -18,70 +18,91 @@ Vector<T> _targetOrBuilder<T>(
     return target;
   } else if (builder != null) {
     return builder(count);
-  } else if (type != null) {
-    return Vector.builder.withType(type)(count);
+  } else if (dataType != null) {
+    return Vector.builder.withType(dataType)(count);
   }
-  throw ArgumentError('Expected either a "target" or a "builder".');
+  throw ArgumentError(
+      'Expected either a "target", a "builder", or a "dataType".');
+}
+
+/// Generic unary operator on a vector.
+Vector<T> unaryOperator<T>(Vector<T> source, T callback(T a),
+    {Vector<T> target, Builder<T> builder, DataType<T> dataType}) {
+  final result = _targetOrBuilderOrDataType(
+      source.count, target, builder, dataType ?? source.dataType);
+  for (var i = 0; i < result.count; i++) {
+    result.setUnchecked(i, callback(source.getUnchecked(i)));
+  }
+  return result;
+}
+
+/// Generic binary operator on two equal sized vectors.
+Vector<T> binaryOperator<T>(
+    Vector<T> sourceA, Vector<T> sourceB, T callback(T a, T b),
+    {Vector<T> target, Builder<T> builder, DataType<T> dataType}) {
+  if (sourceA.count != sourceB.count) {
+    throw ArgumentError('Source vector dimensions do not match.');
+  }
+  final result = _targetOrBuilderOrDataType(
+      sourceA.count, target, builder, sourceA.dataType);
+  for (var i = 0; i < result.count; i++) {
+    result.setUnchecked(
+        i, callback(sourceA.getUnchecked(i), sourceB.getUnchecked(i)));
+  }
+  return result;
 }
 
 /// Adds two numeric vectors [sourceA] and [sourceB].
 Vector<T> add<T extends num>(Vector<T> sourceA, Vector<T> sourceB,
-    {Vector<T> target, Builder<T> builder}) {
-  if (sourceA.count != sourceB.count) {
-    throw ArgumentError('Source vector dimensions do not match.');
-  }
-  final result =
-      _targetOrBuilder(sourceA.count, target, builder, sourceA.dataType);
-  for (var i = 0; i < result.count; i++) {
-    result.setUnchecked(i, sourceA.getUnchecked(i) + sourceB.getUnchecked(i));
-  }
-  return result;
-}
+        {Vector<T> target, Builder<T> builder, DataType<T> dataType}) =>
+    binaryOperator(sourceA, sourceB, (a, b) => a + b,
+        target: target, builder: builder, dataType: dataType);
 
 /// Subtracts two numeric vectors [sourceB] from [sourceA].
 Vector<T> sub<T extends num>(Vector<T> sourceA, Vector<T> sourceB,
-    {Vector<T> target, Builder<T> builder}) {
-  if (sourceA.count != sourceB.count) {
-    throw ArgumentError('Source vector dimensions do not match.');
-  }
-  final result =
-      _targetOrBuilder(sourceA.count, target, builder, sourceA.dataType);
-  for (var i = 0; i < result.count; i++) {
-    result.setUnchecked(i, sourceA.getUnchecked(i) - sourceB.getUnchecked(i));
-  }
-  return result;
-}
+        {Vector<T> target, Builder<T> builder, DataType<T> dataType}) =>
+    binaryOperator(sourceA, sourceB, (a, b) => a - b,
+        target: target, builder: builder, dataType: dataType);
 
 /// Negates a numeric vector [source].
 Vector<T> neg<T extends num>(Vector<T> source,
-    {Vector<T> target, Builder<T> builder}) {
-  final result =
-      _targetOrBuilder(source.count, target, builder, source.dataType);
-  for (var i = 0; i < result.count; i++) {
-    result.setUnchecked(i, -source.getUnchecked(i));
-  }
-  return result;
-}
+        {Vector<T> target, Builder<T> builder, DataType<T> dataType}) =>
+    unaryOperator(source, (a) => -a,
+        target: target, builder: builder, dataType: dataType);
 
 /// Scales a numeric vector [source] with a [factor].
 Vector<T> scale<T extends num>(T factor, Vector<T> source,
-    {Vector<T> target, Builder<T> builder}) {
-  final result =
-      _targetOrBuilder(source.count, target, builder, source.dataType);
-  for (var i = 0; i < result.count; i++) {
-    result.setUnchecked(i, factor * source.getUnchecked(i));
+        {Vector<T> target, Builder<T> builder, DataType<T> dataType}) =>
+    unaryOperator(source, (a) => factor * a,
+        target: target, builder: builder, dataType: dataType);
+
+/// Compares two vectors [sourceA] and [sourceB] with each other.
+bool compare<A, B>(Vector<A> sourceA, Vector<B> sourceB,
+    {bool equals(A a, B b)}) {
+  if (identical(sourceA, sourceB)) {
+    return true;
   }
-  return result;
+  if (sourceA.count != sourceB.count) {
+    return false;
+  }
+  equals ??= (a, b) => a == b;
+  for (var i = 0; i < sourceA.count; i++) {
+    if (!equals(sourceA.getUnchecked(i), sourceB.getUnchecked(i))) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /// Interpolates linearly between [v0] and [v0] with a factor [t].
 Vector<double> lerp<T extends num>(Vector<T> v0, Vector<T> v1, double t,
-    {Vector<double> target, Builder<double> builder}) {
+    {Vector<double> target, Builder<double> builder, DataType<T> dataType}) {
   if (v0.count != v1.count) {
     throw ArgumentError('Source vector dimensions do not match.');
   }
   final t1 = 1.0 - t;
-  final result = _targetOrBuilder(v0.count, target, builder, DataType.float64);
+  final result = _targetOrBuilderOrDataType(
+      v0.count, target, builder, dataType ?? DataType.float64);
   for (var i = 0; i < result.count; i++) {
     result.setUnchecked(i, t1 * v0.getUnchecked(i) + t * v1.getUnchecked(i));
   }
@@ -90,13 +111,13 @@ Vector<double> lerp<T extends num>(Vector<T> v0, Vector<T> v1, double t,
 
 /// Multiplies a numeric [matrix] and a [vector].
 Vector<T> mul<T extends num>(Matrix<T> matrix, Vector<T> vector,
-    {Vector<T> target, Builder<T> builder}) {
+    {Vector<T> target, Builder<T> builder, DataType<T> dataType}) {
   if (matrix.colCount != vector.count) {
     throw ArgumentError('Number of columns in matrix (${matrix.colCount}) '
         'do not match number of elements in vector (${vector.count}).');
   }
-  final result =
-      _targetOrBuilder(matrix.rowCount, target, builder, matrix.dataType);
+  final result = _targetOrBuilderOrDataType(
+      matrix.rowCount, target, builder, dataType ?? matrix.dataType);
   for (var r = 0; r < matrix.rowCount; r++) {
     var sum = result.dataType.nullValue;
     for (var j = 0; j < matrix.colCount; j++) {
@@ -113,7 +134,7 @@ T dot<T extends num>(Vector<T> sourceA, Vector<T> sourceB) {
     throw ArgumentError('Source vector dimensions do not match '
         '(${sourceA.count} and ${sourceB.count}).');
   }
-  var result = 0 as T;
+  var result = sourceA.dataType.nullValue;
   for (var i = 0; i < sourceA.count; i++) {
     result += sourceA.getUnchecked(i) * sourceB.getUnchecked(i);
   }
@@ -134,7 +155,7 @@ double length<T extends num>(Vector<T> source) => math.sqrt(length2(source));
 
 /// Computes the squared length of a vector.
 T length2<T extends num>(Vector<T> source) {
-  var result = 0 as T;
+  var result = source.dataType.nullValue;
   for (var i = 0; i < source.count; i++) {
     final value = source.getUnchecked(i);
     result += value * value;
