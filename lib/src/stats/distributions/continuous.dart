61905109caeecaac1393a3be7e70153c0584ed2c
Lukas Renggli <renggli@gmail.com>
2021-10-17 13:57:11 +0200

    Remove unused code from abstract class (for now)

--- lib/src/stats/distributions/continuous.dart
+++ lib/src/stats/distributions/continuous.dart
@@ -22,35 +22,4 @@ abstract class ContinuousDistribution extends Distribution<double> {
 
   @override
   bool get isUpperBoundOpen => upperBound == double.infinity;
-
-  @override
-  double probability(double x) => derivative(cumulativeProbability, x);
-
-  @override
-  double cumulativeProbability(double x) =>
-      integrate(probability, lowerBound, x);
-
-  @override
-  double inverseCumulativeProbability(num p) {
-    InvalidProbability.check(p);
-    double f(double x) => cumulativeProbability(x) - p;
-    const factor = 10.0;
-    var adjustedLower = lowerBound;
-    var adjustedUpper = upperBound;
-    if (isLowerBoundOpen) {
-      adjustedLower = min(-factor, adjustedUpper);
-      while (f(adjustedLower) > 0.0) {
-        adjustedUpper = adjustedLower;
-        adjustedLower *= factor;
-      }
-    }
-    if (isUpperBoundOpen) {
-      adjustedUpper = max(factor, adjustedLower);
-      while (f(adjustedUpper) < 0.0) {
-        adjustedLower = adjustedUpper;
-        adjustedUpper *= factor;
-      }
-    }
-    return solve(f, adjustedLower, adjustedUpper);
-  }
 }
